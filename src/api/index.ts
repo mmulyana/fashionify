3be282390ef99355d6802948bffe60fdea55e356
mulyant20 <mulyan.t20@gmail.com>
2023-04-11 23:46:03 +0700

    feat: add ssr

---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+const BASE_URL = 'https://fakestoreapi.com'
+
+const REQUEST_URL = {
+  menProduct: `${BASE_URL}/products/category/men's%20clothing`,
+  womanProduct: `${BASE_URL}/products/category/women's%20clothing`,
+}
+
+export { BASE_URL, REQUEST_URL }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,17 +2,17 @@ import { BaseLayout } from '@/components/layout'
 import Image from 'next/image'
 import heroImg from '../assets/images/ospan-ali-unsplash.jpg'
 import { useLoading } from '@/hooks/useLoading'
+import axios from 'axios'
+import { REQUEST_URL } from '@/api'
 
-export default function Home() {
-  const {isLoading} = useLoading()
-  
-  if(!isLoading) {
-    return (
-      <div className='fixed top-0 left-0 w-full h-full bg-red-500'>
+export default function Home({ data }: { data: any[] }) {
+  const { isLoading } = useLoading()
 
-      </div>
-    )
+  if (!isLoading) {
+    return <div className='fixed top-0 left-0 w-full h-full bg-red-500'></div>
   }
+
+  console.log(data)
 
   return (
     <BaseLayout
@@ -20,9 +20,24 @@ export default function Home() {
     >
       <div className='container pt-2'>
         <div className='rounded-2xl h-[440px] relative overflow-hidden from-[#F0F0F1] to-[#F3F4F6] bg-gradient-to-t'>
-          <Image src={heroImg.src} alt='model' className='absolute top-0 right-0 h-full object-fit object-bottom' height={640} width={400} />
+          <Image
+            src={heroImg.src}
+            alt='model'
+            className='absolute top-0 right-0 h-full object-fit object-bottom'
+            height={640}
+            width={400}
+          />
         </div>
       </div>
     </BaseLayout>
   )
+}
+
+export async function getServerSideProps() {
+  const { data } = await axios(REQUEST_URL.womanProduct)
+  return {
+    props: {
+      data,
+    },
+  }
 }
